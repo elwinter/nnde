888c09c526b5ac910a790a3cceb25611bb0b0e0e
Eric Winter <ewinter@gojira.local.stsci.edu>
2020-07-21 14:14:19 -0400

    Cleaned up code, overhauled docstrings.

--- eq/lagaris_01.py
+++ eq/lagaris_01.py
@@ -1,5 +1,6 @@
 """
-Problem 1 in Lagaris et al (1998) (1st order ODE IVP)
+This module implements problem 1 in Lagaris et al (1998) (1st order ODE
+IVP).
 
 Note that an upper-case 'Y' is used to represent the Greek psi from
 the original equation.
@@ -14,14 +15,21 @@ with initial condition:
 
 Y(0) = 1
 
-This equation has the analytical solution for the supplied initial conditions:
+This equation has the analytical solution for the supplied initial
+conditions:
 
 Ya(x) = exp(-x**2)/(1 + x + x**3) + x**2
 
 Reference:
 
-Isaac Elias Lagaris, Aristidis Likas, and Dimitrios I. Fotiadis, "Artificial Neural Networks for Solving Ordinary and Partial Differential Equations", *IEEE Transactions on Neural Networks* **9**(5), pp. 987-999, 1998
+Isaac Elias Lagaris, Aristidis Likas, and Dimitrios I. Fotiadis,
+"Artificial Neural Networks for Solving Ordinary and Partial Differential
+Equations", *IEEE Transactions on Neural Networks* **9**(5), pp. 987-999,
+1998
 
+Todo:
+    * Add function annotations.
+    * Add variable annotations.
 """
 
 
@@ -34,18 +42,22 @@ ic = 1
 
 
 def G(x, Y, dY_dx):
-    """Code for differential equation"""
+    """Compute the differential equation.
+    x is the scalar value of the independent variable.
+    Y is the scalar value of the solution.
+    dY_dx is the scalar value of the 1st derivative of the solution.
+    """
     return (
         dY_dx + (x + (1 + 3*x**2)/(1 + x + x**3))*Y
         - x**3 - 2*x - x**2*(1 + 3*x**2)/(1 + x + x**3)
     )
 
 def dG_dY(x, Y, dY_dx):
-    """Derivative of G wrt Y"""
+    """Derivative of G(xm Ym dY_dx) wrt Y"""
     return x + (1 + 3*x**2)/(1 + x + x**3)
 
 def dG_ddYdx(x, y, dY_dx):
-    """Derivative of G wrt dy/dx"""
+    """Derivative of G(x, Y, dY_dx) wrt dY/dx"""
     return 1
 
 def Ya(x):
@@ -53,7 +65,7 @@ def Ya(x):
     return exp(-x**2/2)/(1 + x + x**3) + x**2
 
 def dYa_dx(x):
-    """Derivative of analytical solution"""
+    """1st derivative of analytical solution"""
     return 2*x - exp(-x**2/2)*(1 + x + 4*x**2 + x**4)/(1 + x + x**3)**2
 
 if __name__ == '__main__':
--- ode1ivp.py
+++ ode1ivp.py
@@ -3,7 +3,33 @@ ODE1IVP - Base class for 1st-order ordinary differential equation initial-
 value problems
 
 This module provides the base functionality for all 1st-order ordinary
-differential equation initial-value problem objects used in the nnode software.
+differential equation initial-value problem objects used in the nnde
+software.
+
+This class is typically used to create an object from the contents of an
+appropriately-designed Python module. The module must define all of the
+variables and functions listed under Attributes.
+
+Since this is a 1st-order ODE, the general form of the equation is:
+
+G(x, Y, dY_dx) = 0
+
+where x is the (scalar) independent variable, Y is the solution,
+and dY_dx is the 1st derivative of the solution.
+
+The initial condition (ic) is the a priori specified value of Y(0).
+
+dG_dY() is the derivative of the differential equation wrt the
+solution Y.
+
+dG_ddYdx() is the derivative of the differential equation wrt the first
+derivative dY/dx.
+
+Ya() is the (optional) analytical solution, used for validating
+results.
+
+dYa_dx() is the (optional) analytical 1st derivative, used for validating
+results.
 
 Example:
     Create an empty ODE1IVP object.
@@ -13,16 +39,19 @@ Example:
 
 Attributes:
     name - String containing name of equation definition module
-    G - Function for equation
-    ic - Scalar for initial condition Y(0)
-    dG_dY - Function for derivative of Gf wrt Y
-    dG_ddYdx - Function for derivative of Gf wrt dY/dx
-    Ya - (Optional) function for analytical solution Ya(x)
-    dYa_dx - (Optional) function for analytical derivative dY_x(x)
+    G(x, Y, dY_dx) - Function for differential equation
+    ic - Scalar for initial condition Y(x=0)
+    dG_dY(x, Y, dY_dx) - Function for derivative of G wrt Y
+    dG_ddYdx(x, Y, dY_dx) - Function for derivative of G wrt dY/dx
+    Ya(x) - (Optional) function for analytical solution
+    dYa_dx(x) - (Optional) function for analytical 1st derivative
 
 Methods:
+    __init__() - Constructor
 
 Todo:
+    * Add function annotations.
+    * Add variable annotations.
 """
 
 
@@ -36,12 +65,14 @@ class ODE1IVP(ODE1):
     value problem objects"""
 
     def __init__(self, diffeqmod=None):
-        """
-        Constructor
+        """Constructor for ODE1IVP objects
+
         Parameters:
         diffeqmod - The name of the Python module containing the problem definition.
         """
         super().__init__()
+
+        # Initialize all attributes.
         self.name = None
         self.G = None
         self.ic = None
@@ -49,17 +80,33 @@ class ODE1IVP(ODE1):
         self.dG_ddYdx = None
         self.Ya = None
         self.dYa_dx = None
+
+        # If a module is specified, use the contents to populate the
+        # attributes of this object.
         if diffeqmod:
+
+            # Save the name of the Python module which defines the equation.
             self.name = diffeqmod
+
+            # Read and validate the equation definition module.
             odemod = import_module(diffeqmod)
             assert odemod.G
             assert odemod.ic is not None
             assert odemod.dG_dY
             assert odemod.dG_ddYdx
+
+            # Save the evaluation function for the differential equation.
             self.G = odemod.G
+
+            # Save the initial condition.
             self.ic = odemod.ic
+
+            # Save the derivatives of the differential equation wrt Y and
+            # dY/dx.
             self.dG_dY = odemod.dG_dY
             self.dG_ddYdx = odemod.dG_ddYdx
+
+            # If provided, save the analytical solution and 1st derivative.
             if odemod.Ya:
                 self.Ya = odemod.Ya
             if odemod.dYa_dx:
@@ -69,5 +116,5 @@ class ODE1IVP(ODE1):
 if __name__ == '__main__':
     ode1ivp = ODE1IVP()
     print(ode1ivp)
-    ode1ivp = ODE1IVP('lagaris_01')
+    ode1ivp = ODE1IVP('eq.lagaris_01')
     print(ode1ivp)
