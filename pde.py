fd11895f07f532b712f551f38bc0697b032b72e6
Eric Winter <ewinter@gojira.local>
2020-07-23 19:40:37 -0400

    Updated docstrings. Changed base class name.

--- pde.py
+++ pde.py
@@ -4,6 +4,9 @@ PDE - Base class for partial differential equations
 This module provides the base functionality for all partial differential
 equation objects used in the nnode software.
 
+This class is currently an abstract base class. It must be subclassed    
+to be useful.
+
 Example:
     Create an empty PDE object.
         pde = PDE()
@@ -12,20 +15,27 @@ Attributes:
     None
 
 Methods:
-    None
+    __init__() - Constructor
 
 Todo:
-    None
+    * Add function annotations.
+    * Add variable annotations.
 """
 
 
-from diffeq import DiffEq
+__all__ = []
+__version__ = '0.0'
+__author__ = 'Eric Winter (ewinter@stsci.edu)'
 
 
-class PDE(DiffEq):
-    """Base class for all partial differential equation objects"""
+from differentialequation import DifferentialEquation
+
+
+class PDE(DifferentialEquation):
+    """Abstract base class for all partial differential equation objects"""
 
     def __init__(self):
+        """Constructor for PDE objects - must be  overridden in subclass."""
         super().__init__()
 
 
