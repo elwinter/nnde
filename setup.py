21366399dfc5dfd8a009950e3e21ca2a9738c137
Eric Winter <ewinter@gojira.local.stsci.edu>
2020-08-16 16:37:54 -0400

    PEP8 compliance tweaks.

--- setup.py
+++ setup.py
@@ -8,14 +8,16 @@ setuptools.setup(
     version="0.0.3",
     author="Eric Winter",
     author_email="ewinter@stsci.edu",
-    description="A package implementing a collection of neural networks to solve ordinary and partial differential equations",
+    description=("A package implementing a collection of neural networks to"
+                 " solve ordinary and partial differential equations"),
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/elwinter/nnde",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        ("License :: OSI Approved :: GNU Lesser General Public License v3"
+         "(LGPLv3)"),
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
