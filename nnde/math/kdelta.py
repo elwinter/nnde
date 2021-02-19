af4aebb7834ef2c11cf1ae3394d6c7be11d5ee6a
Eric Winter <eric.winter@jhuapl.edu>
2021-02-19 13:44:42 -0500

    Moved kdelta test code to test_kdelta.py.

--- nnde/math/kdelta.py
+++ nnde/math/kdelta.py
@@ -7,29 +7,13 @@ kdelta(i, j) = 1 if i == j, else 0
 Example:
     Calculate the Kronecker delta for 2 integers.
         kd = kdelta.kdelta(i, j)
-
-To do:
-    * Add function annotations.
-    * Add variable annotations.
 """
 
 
 def kdelta(i: int, j: int) -> int:
-    """
-    The Konecker delta function.
+    """The Konecker delta function
 
     Given 2 integers i and j, return 1 if i == j, else 0.
     """
 
     return 1 if i == j else 0
-
-
-if __name__ == '__main__':
-    for i in range(-2, 3):
-        for j in range(-2, 3):
-            print("Testing kdelta[%d, %d]." % (i, j))
-            kd = kdelta(i, j)
-            if i == j:
-                assert kd == 1
-            else:
-                assert kd == 0
---
+++ nnde/math/tests/test_kdelta.py
@@ -0,0 +1,16 @@
+import unittest
+
+from nnde.math.kdelta import kdelta
+
+class TestBuilder(unittest.TestCase):
+
+    def test_kdelta(self):
+        for i in range(-2, 3):
+            for j in range(-2, 3):
+                if i == j:
+                    self.assertEqual(kdelta(i, j), 1)
+                else:
+                    self.assertEqual(kdelta(i, j), 0)
+
+if __name__ == '__main__':
+    unittest.main()
