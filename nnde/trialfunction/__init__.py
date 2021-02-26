5cee8d92e5da015c44e69cc83611954411004416
Eric Winter <eric.winter@jhuapl.edu>
2021-02-26 15:54:05 -0500

    Updated __init__.py in nnde.trialfunctions tree.

---
+++ nnde/trialfunction/__init__.py
@@ -0,0 +1,15 @@
+"""nnde.trialfunction
+
+Python package to for trial functions in the nnde package.
+
+Modules
+-------
+diff1dtrialfunction.py
+diff2dtrialfunction.py
+diff3dtrialfunction.py
+trialfunction.py
+
+Packages
+--------
+tests
+"""
---
+++ nnde/trialfunction/tests/__init__.py
@@ -0,0 +1,9 @@
+"""nnde.trialfunction.tests
+
+Tests for Python package to for trial functions in the nnde package.
+
+Modules
+-------
+test_Diff1DTrialFunction.py
+test_TrialFunction.py
+"""
