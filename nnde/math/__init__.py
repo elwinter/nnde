6c6f8b56bc719f2d9ca7c407775954d9cf4bf2bf
Eric Winter <eric.winter@jhuapl.edu>
2021-02-26 15:49:22 -0500

    Updated __init__.py files in nnde.math tree.

---
+++ nnde/math/__init__.py
@@ -0,0 +1,14 @@
+"""nnde.math
+
+Python package to for math utility functions in the nnde package.
+
+Modules
+-------
+kdelta.py
+sigma.py
+trainingdata.py
+
+Packages
+--------
+tests
+"""
---
+++ nnde/math/tests/__init__.py
@@ -0,0 +1,10 @@
+"""nnde.math.tests
+
+Tests for Python package to for math utility functions in the nnde package.
+
+Modules
+-------
+test_kdelta.py
+test_sigma.py
+test_trainingdata.py
+"""
