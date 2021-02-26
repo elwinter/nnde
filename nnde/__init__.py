3f308a38871e911761c8ad13f60b00d369a428af
Eric Winter <eric.winter@jhuapl.edu>
2021-02-26 15:39:27 -0500

    Removed top-level tests directory.

---
+++ nnde/__init__.py
@@ -0,0 +1,12 @@
+"""nnde
+
+Python package to solve ordinary and partial differential equations using
+neural networks.
+
+Packages
+--------
+differentialequation
+math
+neuralnetwork
+trialfunction
+"""
