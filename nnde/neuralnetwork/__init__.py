3217513e2f42f3d8cb81d8f491381cd745a9e682
Eric Winter <eric.winter@jhuapl.edu>
2021-02-26 15:51:51 -0500

    Updated __init__.py in nnde.neuralnetwork tree.

---
+++ nnde/neuralnetwork/__init__.py
@@ -0,0 +1,16 @@
+"""nnde.neuralnetwork
+
+Python package to for neural networks in the nnde package.
+
+Modules
+-------
+neuralnetwork.py
+nnode1ivp.py
+nnpde1ivp.py
+nnpde2diff.py
+slffnn.py
+
+Packages
+--------
+tests
+"""
---
+++ nnde/neuralnetwork/tests/__init__.py
@@ -0,0 +1,7 @@
+"""nnde.neuralnetwork.tests
+
+Tests for Python package to for neural networks in the nnde package.
+
+Modules
+-------
+"""
