1155ed5137016251f6ef4ad2bb2fa3c5dc8243f1
Eric Winter <eric.winter@jhuapl.edu>
2021-03-01 21:08:10 -0500

    Updated docstrings for DifferentialEquation.

--- nnde/differentialequation/differentialequation.py
+++ nnde/differentialequation/differentialequation.py
@@ -1,17 +1,17 @@
-"""DifferentialEquation - Base class for differential equations
+"""Abstract base class for differential equations
 
-This module provides the base functionality for all differential equation
+This module defines the methods required for all differential equation
 objects used in the nnde software.
 
-This class is currently an abstract base class. It must be subclassed
-to be useful.
+This class is an abstract base class. It must be subclassed to be useful. All
+methods in this class will raise NNDEException if called.
 
 Attributes:
     None
 
 Methods:
     __init__() - Constructor
-    G - Function for differential equation, in the form G() = 0,
+    G() - Function for differential equation, in the form G() = 0,
     where G() is a function of the independent variables x, the solution
     Y(x), and its derivatives.
 """
@@ -21,17 +21,12 @@ from nnde.exceptions.nndeexception import NNDEException
 
 
 class DifferentialEquation:
-    """Abstract base class for all differential equation objects
-    
-    Since this is an abstract class, it must not be instantiated. All methods
-    will raise an Exception.
-    """
+    """Abstract base class for differential equation objects"""
 
     def __init__(self, *args, **kwargs):
         """Constructor for DifferentialEquation objects"""
         raise NNDEException
 
-    def G(self, *args, **kwargs):
-        """Differential equation to be solved, in the form G() = 0
-        Must be overridden in subclass."""
+    def G(self, *args, **kwargs) -> float:
+        """Differential equation implementation"""
         raise NNDEException
