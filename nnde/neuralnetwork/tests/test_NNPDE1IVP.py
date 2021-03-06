fb0de9e05c65c858a394e7d9584f58b2e84b956b
Eric Winter <eric.winter@jhuapl.edu>
2021-03-06 16:02:28 -0500

    Added remaining test stubs.

---
+++ nnde/neuralnetwork/tests/test_NNPDE1IVP.py
@@ -0,0 +1,14 @@
+import unittest
+
+from nnde.neuralnetwork.nnpde1ivp import NNPDE1IVP
+
+
+class TestNNPDE1IVP(unittest.TestCase):
+
+    def test___init__(self):
+        pass
+        # NNPDE1IVP()
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ nnde/neuralnetwork/tests/test_PDE2DIFF.py
@@ -0,0 +1,13 @@
+import unittest
+
+from nnde.neuralnetwork.nnpde2diff import NNPDE2DIFF
+
+
+class TestNNPDE2DIFF(unittest.TestCase):
+
+    def test___init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
