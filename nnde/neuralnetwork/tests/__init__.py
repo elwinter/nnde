9f286de52998c3d04f620714963255217ef3e136
Eric Winter <eric.winter@jhuapl.edu>
2021-03-06 15:59:54 -0500

    Updated to include all test modules.

--- nnde/neuralnetwork/tests/__init__.py
+++ nnde/neuralnetwork/tests/__init__.py
@@ -4,4 +4,9 @@ Tests for Python package to for neural networks in the nnde package.
 
 Modules
 -------
+test_NeuralNetwork.py
+test_NNODE1IVP.py
+test_NNPDE1IVP.py
+test_NNPDE2DIFF.py
+test_SLFFNN.py
 """
