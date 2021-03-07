d3adb898edddcc11dfd395dfa449ccaf1061f8e1
Eric Winter <eric.winter@jhuapl.edu>
2021-03-07 13:19:10 -0500

    Moved self-test code from nnode1ivp.py to demo project. Added stubs to test script for other methods.

--- nnde/neuralnetwork/tests/test_NNODE1IVP.py
+++ nnde/neuralnetwork/tests/test_NNODE1IVP.py
@@ -8,6 +8,39 @@ class TestNNODE1IVP(unittest.TestCase):
     def test___init__(self):
         NNODE1IVP()
 
+    def test___str__(self):
+        pass
+
+    def test_train(self):
+        pass
+
+    def test_run(self):
+        pass
+
+    def test_run_derivative(self):
+        pass
+
+    def test__Yt(self):
+        pass
+
+    def test__dYt_dx(self):
+        pass
+
+    def test__train_delta(self):
+        pass
+
+    def test__train_minimize(self):
+        pass
+
+    def test__compute_error(self):
+        pass
+
+    def test__compute_error_gradient(self):
+        pass
+
+    def test__print_progress(self):
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()
