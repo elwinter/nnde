2e23fb4a69998595126e15f4685583f307d5fcf0
Eric Winter <eric.winter@jhuapl.edu>
2021-03-07 15:28:23 -0500

    Updated __init__.py files for module renamings.

--- nnde/differentialequation/examples/__init__.py
+++ nnde/differentialequation/examples/__init__.py
@@ -5,13 +5,13 @@ Example differential equations for the nnde package.
 Modules
 -------
 diff1d_half.py
-diff1d_halfsine+increase.py
+diff1d_halfsine_increase.py
 diff1d_halfsine.py
 diff1d_one.py
 diff1d_zero.py
-diff2d_halfsine+increase.py
+diff2d_halfsine_increase.py
 diff2d_halfsine.py
-diff3d_halfsine+increase.py
+diff3d_halfsine_increase.py
 diff3d_halfsine.py
 example_pde1ivp_01.py
 lagaris_01.py
--- nnde/differentialequation/examples/tests/__init__.py
+++ nnde/differentialequation/examples/tests/__init__.py
@@ -5,14 +5,14 @@ Tests for example differential equations for the nnde package.
 Modules
 -------
 test_diff1d_half.py
+test_diff1d_halfsine_increase.py
 test_diff1d_halfsine.py
-test_diff1d_halfsine+increase.py
 test_diff1d_one.py
 test_diff1d_zero.py
+test_diff2d_halfsine_increase.py
 test_diff2d_halfsine.py
-test_diff2d_halfsine+increase.py
+test_diff3d_halfsine_increase.py
 test_diff3d_halfsine.py
-test_diff3d_halfsine+increase.py
 test_example_pde1ivp.py
 test_lagaris_01.py
 test_lagaris_02.py
