c52fc657d395d7fa1d5f999a1cea66ead6918d34
Eric Winter <eric.winter@jhuapl.edu>
2021-03-07 13:32:21 -0500

    Started upgrade of nnpde2diff.py.

--- nnde/neuralnetwork/tests/test_PDE2DIFF.py
+++ nnde/neuralnetwork/tests/test_PDE2DIFF.py
@@ -8,6 +8,38 @@ class TestNNPDE2DIFF(unittest.TestCase):
     def test___init__(self):
         pass
 
+    def test_train(self):
+        pass
+
+    def test_run(self):
+        pass
+
+    def test_run_gradient(self):
+        pass
+
+    def test_run_laplacian(self):
+        pass
+
+    def test___str__(self):
+        pass
+
+    def test__train_delta(self):
+        pass
+
+    def test__train_minimize(self):
+        pass
+
+    def test__compute_error(self):
+        pass
+
+    def test__compute_error_jacobian(self):
+        pass
+
+    def test__iteration_callback(self):
+        pass
+
+    
+
 
 if __name__ == '__main__':
     unittest.main()
