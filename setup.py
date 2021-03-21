0e3fa1ff48fac3ac8c690a16b1f9ff5961d46f5f
Eric Winter <eric.winter@jhuapl.edu>
2021-03-21 17:07:00 -0400

    Version number to 0.0.7.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="nnde",
-    version="0.0.6",
+    version="0.0.7",
     author="Eric Winter",
     author_email="eric.winter62@gmail.com",
     description=("A package implementing a collection of neural networks to"
