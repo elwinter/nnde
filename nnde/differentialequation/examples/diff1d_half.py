99b43e847c7c39b25dabda059dc5aec3ef68a7d6
Eric Winter <eric.winter62@gmail.com>
2021-08-15 15:46:51 -0400

    Docstring updates.

--- nnde/differentialequation/examples/diff1d_half.py
+++ nnde/differentialequation/examples/diff1d_half.py
@@ -3,7 +3,7 @@
 This module implements a 1-D diffusion PDE.
 
 Note that an upper-case 'Y' is used to represent the Greek psi, which
-represents the problem solution Y(x,t). Y(x, t) is normalized to the
+represents the problem solution Y(x, t). Y(x, t) is normalized to the
 range [0, 1], and is treated as unitless.
 
 The equation is defined on the domain:
@@ -45,9 +45,6 @@ Eric Winter (eric.winter62@gmail.com)
 """
 
 
-import numpy as np
-
-
 # Diffusion coefficient (L**2/T, where L is a length, and T is a time).
 D = 0.1
 
@@ -56,8 +53,8 @@ C = 0.5
 
 
 def G(xt, Y, delY, del2Y):
-    """The differential equation in standard form.
-    
+    """Compute the differential equation in standard form.
+
     Compute the value of the differential equation in standard form. For a
     perfect solution, the value should be 0.
 
@@ -71,28 +68,27 @@ def G(xt, Y, delY, del2Y):
         Values for dY/dx and dY/dt, in that order.
     del2Y : array-like of 2 float
         Values for d2Y/dx2 and d2Y/dt2, in that order.
-    
+
     Returns
     -------
     result : float
         Value of the differential equation.
     """
-    (x, t) = xt
-    (dY_dx, dY_dt) = delY
-    (d2Y_dx2, d2Y_dt2) = del2Y
+    dY_dt = delY[1]
+    d2Y_dx2 = del2Y[0]
     return dY_dt - D*d2Y_dx2
 
 
 def f0(xt):
     """Boundary condition at (x, t) = (0, t).
-    
+
     Compute the value of the solution at (x, t) = (0, t).
 
     Parameters
     ----------
     xt : array-like of 2 float
         Values for x and t, in that order.
-    
+
     Returns
     -------
     result : float
@@ -103,14 +99,14 @@ def f0(xt):
 
 def f1(xt):
     """Boundary condition at (x, t) = (1, t).
-    
+
     Compute the value of the solution at (x, t) = (1, t).
 
     Parameters
     ----------
     xt : array-like of 2 float
         Values for x and t, in that order.
-    
+
     Returns
     -------
     result : float
@@ -120,153 +116,530 @@ def f1(xt):
 
 
 def Y0(xt):
-    """Boundary condition at (x,t) = (x,0)"""
+    """Boundary condition at (x, t) = (x, 0).
+
+    Compute the value of the solution at (x, t) = (x, 0).
+
+    Parameters
+    ----------
+    xt : array-like of 2 float
+        Values for x and t, in that order.
+
+    Returns
+    -------
+    result : float
+        Value of the solution at (x, t) = (x, 0).
+    """
     return C
 
 
+# Gather the boundary condition functions in a single array.
 bc = [[f0, f1], [Y0, None]]
 
 
 def df0_dx(xt):
-    """1st derivative of BC wrt x at (x,t) = (0,t)"""
+    """1st derivative of BC wrt x at (x, t) = (0, t).
+
+    Compute the 1st derivative of BC wrt x at (x, t) = (0, t).
+
+    Parameters
+    ----------
+    xt : array-like of 2 float
+        Values for x and t, in that order.
+
+    Returns
+    -------
+    result : float
+        Value of the 1st derivative of BC wrt x at (x, t) = (0, t).
+    """
     return 0
 
 
 def df0_dt(xt):
-    """1st derivative of BC wrt t at (x,t) = (0,t)"""
+    """1st derivative of BC wrt t at (x, t) = (0, t).
+
+    Compute the 1st derivative of BC wrt t at (x, t) = (0, t).
+
+    Parameters
+    ----------
+    xt : array-like of 2 float
+        Values for x and t, in that order.
+
+    Returns
+    -------
+    result : float
+        Value of the 1st derivative of BC wrt t at (x, t) = (0, t).
+    """
     return 0
 
 
 def df1_dx(xt):
-    """1st derivative of BC wrt x at (x,t) = (1,t)"""
+    """1st derivative of BC wrt x at (x, t) = (1, t).
+
+    Compute the 1st derivative of BC wrt x at (x, t) = (1, t).
+
+    Parameters
+    ----------
+    xt : array-like of 2 float
+        Values for x and t, in that order.
+
+    Returns
+    -------
+    result : float
+        Value of the 1st derivative of BC wrt x at (x, t) = (1, t).
+    """
     return 0
 
 
 def df1_dt(xt):
-    """1st derivative of BC wrt t at (x,t) = (1,t)"""
+    """1st derivative of BC wrt t at (x, t) = (1, t).
+
+    Compute the 1st derivative of BC wrt t at (x, t) = (1, t).
+
+    Parameters
+    ----------
+    xt : array-like of 2 float
+        Values for x and t, in that order.
+
+    Returns
+    -------
+    result : float
+        Value of the 1st derivative of BC wrt t at (x, t) = (1, t).
+    """
     return 0
 
 
 def dY0_dx(xt):
-    """1st derivative of BC wrt x at (x,t) = (x,0)"""
+    """1st derivative of BC wrt x at (x, t) = (x, 0).
+
+    Compute the 1st derivative of BC wrt x at (x, t) = (x, 0).
+
+    Parameters
+    ----------
+    xt : array-like of 2 float
+        Values for x and t, in that order.
+
+    Returns
+    -------
+    result : float
+        Value of the 1st derivative of BC wrt x at (x, t) = (x, 0).
+    """
     return 0
 
 
 def dY0_dt(xt):
-    """1st derivative of BC wrt t at (x,t) = (x,0)"""
+    """1st derivative of BC wrt t at (x, t) = (x, 0).
+
+    Compute the 1st derivative of BC wrt t at (x, t) = (x, 0).
+
+    Parameters
+    ----------
+    xt : array-like of 2 float
+        Values for x and t, in that order.
+
+    Returns
+    -------
+    result : float
+        Value of the 1st derivative of BC wrt t at (x, t) = (x, 0).
+    """
     return 0
 
 
+# Gather the gradient functions into a single array.
 delbc = [[[df0_dx, df0_dt], [df1_dx, df1_dt]],
          [[dY0_dx, dY0_dt], [None, None]]]
 
 
 def d2f0_dx2(xt):
-    """2nd derivative of BC wrt x at (x,t) = (0,t)"""
+    """2nd derivative of BC wrt x at (x, t) = (0, t).
+
+    Compute the 2nd derivative of BC wrt x at (x, t) = (0, t).
+
+    Parameters
+    ----------
+    xt : array-like of 2 float
+        Values for x and t, in that order.
+
+    Returns
+    -------
+    result : float
+        Value of the 2ns derivative of BC wrt x at (x, t) = (0, t).
+    """
     return 0
 
 
 def d2f0_dt2(xt):
-    """2nd derivative of BC wrt t at (x,t) = (0,t)"""
+    """2nd derivative of BC wrt t at (x, t) = (0, t).
+
+    Compute the 2nd derivative of BC wrt t at (x, t) = (0, t).
+
+    Parameters
+    ----------
+    xt : array-like of 2 float
+        Values for x and t, in that order.
+
+    Returns
+    -------
+    result : float
+        Value of the 2ns derivative of BC wrt t at (x, t) = (0, t).
+    """
     return 0
 
 
 def d2f1_dx2(xt):
-    """2nd derivative of BC wrt x at (x,t) = (1,t)"""
+    """2nd derivative of BC wrt x at (x, t) = (1, t).
+
+    Compute the 2nd derivative of BC wrt x at (x, t) = (1, t).
+
+    Parameters
+    ----------
+    xt : array-like of 2 float
+        Values for x and t, in that order.
+
+    Returns
+    -------
+    result : float
+        Value of the 2ns derivative of BC wrt x at (x, t) = (1, t).
+    """
     return 0
 
 
 def d2f1_dt2(xt):
-    """2nd derivative of BC wrt t at (x,t) = (1,t)"""
+    """2nd derivative of BC wrt t at (x, t) = (1, t).
+
+    Compute the 2nd derivative of BC wrt t at (x, t) = (1, t).
+
+    Parameters
+    ----------
+    xt : array-like of 2 float
+        Values for x and t, in that order.
+
+    Returns
+    -------
+    result : float
+        Value of the 2ns derivative of BC wrt t at (x, t) = (1, t).
+    """
     return 0
 
 
 def d2Y0_dx2(xt):
-    """2nd derivative of BC wrt x at (x,t) = (x,0)"""
+    """2nd derivative of BC wrt x at (x, t) = (x, 0).
+
+    Compute the 2nd derivative of BC wrt x at (x, t) = (x, 0).
+
+    Parameters
+    ----------
+    xt : array-like of 2 float
+        Values for x and t, in that order.
+
+    Returns
+    -------
+    result : float
+        Value of the 2ns derivative of BC wrt x at (x, t) = (x, 0).
+    """
     return 0
 
 
 def d2Y0_dt2(xt):
-    """2nd derivative of BC wrt t at (x,t) = (x,0)"""
+    """2nd derivative of BC wrt t at (x, t) = (x, 0).
+
+    Compute the 2nd derivative of BC wrt t at (x, t) = (x, 0).
+
+    Parameters
+    ----------
+    xt : array-like of 2 float
+        Values for x and t, in that order.
+
+    Returns
+    -------
+    result : float
+        Value of the 2ns derivative of BC wrt x at (x, t) = (t, 0).
+    """
     return 0
 
 
+# Gather the functions for the Laplacian components into a single array.
 del2bc = [[[d2f0_dx2, d2f0_dt2], [d2f1_dx2, d2f1_dt2]],
           [[d2Y0_dx2, d2Y0_dt2], [None, None]]]
 
 
 def dG_dY(xt, Y, delY, del2Y):
-    """Partial of PDE wrt Y"""
+    """1st derivative of G wrt Y.
+
+    Compute the 1st derivative of G wrt Y.
+
+    Parameters
+    ----------
+    xt : array-like of 2 float
+        Values for x and t, in that order.
+    Y : float
+        Current solution value.
+    delY : array-like of 2 float
+        Values for dY/dx and dY/dt, in that order.
+    del2Y : array-like of 2 float
+        Values for d2Y/dx2 and d2Y/dt2, in that order.
+
+    Returns
+    -------
+    result : float
+        Value of dG/dY.
+    """
     return 0
 
 
 def dG_dY_dx(xt, Y, delY, del2Y):
-    """Partial of PDE wrt dY/dx"""
+    """1st derivative of G wrt dY/dx.
+
+    Compute the 1st derivative of G wrt dY/dx.
+
+    Parameters
+    ----------
+    xt : array-like of 2 float
+        Values for x and t, in that order.
+    Y : float
+        Current solution value.
+    delY : array-like of 2 float
+        Values for dY/dx and dY/dt, in that order.
+    del2Y : array-like of 2 float
+        Values for d2Y/dx2 and d2Y/dt2, in that order.
+
+    Returns
+    -------
+    result : float
+        Value of dG/d(dY/dx).
+    """
     return 0
 
 
 def dG_dY_dt(xt, Y, delY, del2Y):
-    """Partial of PDE wrt dY/dt"""
+    """1st derivative of G wrt dY/dt.
+
+    Compute the 1st derivative of G wrt dY/dt.
+
+    Parameters
+    ----------
+    xt : array-like of 2 float
+        Values for x and t, in that order.
+    Y : float
+        Current solution value.
+    delY : array-like of 2 float
+        Values for dY/dx and dY/dt, in that order.
+    del2Y : array-like of 2 float
+        Values for d2Y/dx2 and d2Y/dt2, in that order.
+
+    Returns
+    -------
+    result : float
+        Value of dG/d(dY/dt).
+    """
     return 1
 
 
+# Gather the derivatives into a single array.
 dG_ddelY = [dG_dY_dx, dG_dY_dt]
 
 
 def dG_d2Y_dx2(xt, Y, delY, del2Y):
-    """Partial of PDE wrt d2Y/dx2"""
+    """1st derivative of G wrt d2Y/dx2.
+
+    Compute the 1st derivative of G wrt d2Y/dx2.
+
+    Parameters
+    ----------
+    xt : array-like of 2 float
+        Values for x and t, in that order.
+    Y : float
+        Current solution value.
+    delY : array-like of 2 float
+        Values for dY/dx and dY/dt, in that order.
+    del2Y : array-like of 2 float
+        Values for d2Y/dx2 and d2Y/dt2, in that order.
+
+    Returns
+    -------
+    result : float
+        Value of dG/d(d2Y/dx2).
+    """
     return -D
 
 
 def dG_d2Y_dt2(xt, Y, delY, del2Y):
-    """Partial of PDE wrt d2Y/dt2"""
+    """1st derivative of G wrt d2Y/dt2.
+
+    Compute the 1st derivative of G wrt d2Y/dt2.
+
+    Parameters
+    ----------
+    xt : array-like of 2 float
+        Values for x and t, in that order.
+    Y : float
+        Current solution value.
+    delY : array-like of 2 float
+        Values for dY/dx and dY/dt, in that order.
+    del2Y : array-like of 2 float
+        Values for d2Y/dx2 and d2Y/dt2, in that order.
+
+    Returns
+    -------
+    result : float
+        Value of dG/d(d2Y/dt2).
+    """
     return 0
 
 
+# Gather the derivatives into a single array.
 dG_ddel2Y = [dG_d2Y_dx2, dG_d2Y_dt2]
 
 
 def A(xt):
-    """Optimized version of boundary condition function"""
+    """Optimized version of the boundary condition function.
+
+    Compute the optimized version of the boundary condition function.
+
+    Parameters
+    ----------
+    xt : array-like of 2 float
+        Values for x and t, in that order.
+
+    Returns
+    -------
+    result : float
+        Value of the boundary condition function.
+    """
     return C
 
 
 def delA(xt):
-    """Optimized version of boundary condition function gradient"""
+    """Gradient of optimized version of the boundary condition function.
+
+    Compute the gradient of the optimized version of the boundary condition
+    function.
+
+    Parameters
+    ----------
+    xt : array-like of 2 float
+        Values for x and t, in that order.
+
+    Returns
+    -------
+    result : float
+        Value of the boundary condition function gradient.
+    """
     return [0, 0]
 
 
 def del2A(xt):
-    """Optimized version of boundary condition function Laplacian"""
+    """Laplacian components of optimized boundary condition function.
+
+    Compute the Laplacian components of the optimized the boundary condition
+    function.
+
+    Parameters
+    ----------
+    xt : array-like of 2 float
+        Values for x and t, in that order.
+
+    Returns
+    -------
+    result : float
+        Value of the boundary condition function Laplacian components.
+    """
     return [0, 0]
 
 
 def Ya(xt):
-    """Analytical solution"""
+    """Analytical solution of G.
+
+    Compute the analytical solution of G.
+
+    Parameters
+    ----------
+    xt : array-like of 2 float
+        Values for x and t, in that order.
+
+    Returns
+    -------
+    result : float
+        Value of the analytical solution.
+    """
     return C
 
 
 def dYa_dx(xt):
-    """Analytical x-gradient"""
+    """1st x-derivative of the analytical solution of G.
+
+    Compute the 1st x-derivative of the analytical solution of G.
+
+    Parameters
+    ----------
+    xt : array-like of 2 float
+        Values for x and t, in that order.
+
+    Returns
+    -------
+    result : float
+        Value of the 1st x-derivative of the analytical solution of G.
+    """
     return 0
 
 
 def dYa_dt(xt):
-    """Analytical t-gradient"""
+    """1st t-derivative of the analytical solution of G.
+
+    Compute the 1st t-derivative of the analytical solution of G.
+
+    Parameters
+    ----------
+    xt : array-like of 2 float
+        Values for x and t, in that order.
+
+    Returns
+    -------
+    result : float
+        Value of the 1st t-derivative of the analytical solution of G.
+    """
     return 0
 
 
+# Gather the analytical gradient functions into a single array.
 delYa = [dYa_dx, dYa_dt]
 
 
 def d2Ya_dx2(xt):
-    """Analytical x-Laplacian"""
+    """2nd x-derivative of the analytical solution of G.
+
+    Compute the 2nd x-derivative of the analytical solution of G.
+
+    Parameters
+    ----------
+    xt : array-like of 2 float
+        Values for x and t, in that order.
+
+    Returns
+    -------
+    result : float
+        Value of the 2nd x-derivative of the analytical solution of G.
+    """
     return 0
 
 
 def d2Ya_dt2(xt):
-    """Analytical t-Laplacian"""
+    """2nd t-derivative of the analytical solution of G.
+
+    Compute the 2nd t-derivative of the analytical solution of G.
+
+    Parameters
+    ----------
+    xt : array-like of 2 float
+        Values for x and t, in that order.
+
+    Returns
+    -------
+    result : float
+        Value of the 2nd t-derivative of the analytical solution of G.
+    """
     return 0
 
 
+# Gather the Laplacian component functions into a single array.
 del2Ya = [d2Ya_dx2, d2Ya_dt2]
